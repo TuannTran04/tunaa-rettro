a3ed4d28358da27c8a0f5dab132f94a11bad21df
Trần Quốc Tuấn <89908087+TuannTran04@users.noreply.github.com>
2022-06-09 16:18:22 +0700

    Update code

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@ const menuShow = document.querySelector('.nav_icon a .menu');
 const menuClose = document.querySelector('.nav_icon a .close');
 const info = document.querySelector('.nav_info');
 const arrowUp = document.querySelector('.arrow-up');
-const navHeader = document.querySelector('.nav_contain');
+const navHeader = document.querySelector('.nav_header');
+const navHeaderHide = document.querySelector('.nav_contain.hide')
+
 
 // Menu show
 menuShow.onclick = function (e) {
     e.preventDefault();
-    info.classList.toggle('nav_info_show');
-    e.target.classList.remove('active');
-    menuClose.classList.add('active');
+        info.classList.toggle('nav_info_show');
+        e.target.classList.remove('active');
+        menuClose.classList.add('active');
 }
 
 menuClose.onclick = function (e) {
@@ -21,13 +23,13 @@ menuClose.onclick = function (e) {
 
 // Arrow up
 window.addEventListener('scroll', function (e) {
-    if(window.scrollY > 600) {
-        Object.assign(arrowUp.style, {  
+    if (window.scrollY > 600) {
+        Object.assign(arrowUp.style, {
             visibility: 'visible',
             opacity: 1,
         });
     } else {
-        Object.assign(arrowUp.style, {  
+        Object.assign(arrowUp.style, {
             visibility: 'hidden',
             opacity: 0,
         });
@@ -35,16 +37,18 @@ window.addEventListener('scroll', function (e) {
 })
 
 arrowUp.onclick = function (e) {
-    window.scroll({top: 0, behavior: "smooth"});
+    window.scroll({ top: 0, behavior: "smooth" });
 }
 
 // Header show
 window.addEventListener('scroll', function (e) {
-    if(window.scrollY > 300 && window.scrollY < 700) {
+    if (window.scrollY > 300 && window.scrollY < 700) {
         navHeader.classList.add('hide');
-    } 
+        info.classList.add('hide');
+    }
     else {
         navHeader.classList.remove('hide');
+        info.classList.remove('hide');
     }
 })
 
